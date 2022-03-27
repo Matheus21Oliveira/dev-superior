d64e0bb4aa17a90a7f65380317511baedd588bfd
Matheus21Oliveira <matheus21oliveira@gmail.com>
2022-03-27 16:02:53 -0300

    exercicio de repetição

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,28 +3,18 @@
 namespace ConsoleApp1{
     class Program {
         static void Main(String[] args){
-            
-            Console.WriteLine("Digite três números");
-            int n1 = int.Parse(Console.ReadLine());
-            int n2 = int.Parse(Console.ReadLine());
-            int n3 = int.Parse(Console.ReadLine());
+            Console.Write("Quantos números inteiros você vai digitar? ");
+            int n = int.Parse(Console.ReadLine());
 
-            double resultado = Maior(n1, n2, n3); 
+            int soma = 0;
 
-            Console.WriteLine("Maior = " + resultado);
-        }  
-        
-        static int Maior(int a, int b, int c) { 
-            int m; 
-
-            if (a > b && a > c) {
-                m = a;
-            } else if (b > c) {
-                m = b;
-            } else {
-                m = c;
+            for (int i = 1; i <= n; i++) {
+                Console.Write("Valor {0}: ", i);
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
             }
-            return m;
+
+            Console.WriteLine("soma = " + soma);
         }
     }
 }
