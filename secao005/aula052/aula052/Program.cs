4953f4e68a0b69601cf3ba606dcca778570c441b
matheus21oliveira <matheus21oliveira@gmail.com>
2022-04-16 20:01:33 -0300

    exercicios da secao 51

---
+++ secao005/aula052/aula052/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+namespace aula052 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Produto produto = new Produto(nome, preco);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + produto);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+
+            produto.AdicionarProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + produto);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+
+            produto.RemoverProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + produto);
+        }
+    }
+}
