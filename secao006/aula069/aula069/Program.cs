19ea06f69a002439757d090e959ad18e4a686e62
matheus21oliveira <matheus21oliveira@gmail.com>
2022-04-19 23:31:12 -0300

    exercicios de aula

---
+++ secao006/aula069/aula069/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace aula069 {
+    class Program {
+        static void Main(string[] args) {
+            int numero = int.Parse(Console.ReadLine());
+
+            double[] vetor = new double[numero];
+
+            for (int i = 0; i < numero; i++) {
+                vetor[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            double soma = 0;
+
+            for (int i = 0; i < vetor.Length; i++) {
+                soma += vetor[i];
+            }
+
+            double media = soma / numero;
+
+            Console.WriteLine("AVERAGE HEIGHT = " + media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
