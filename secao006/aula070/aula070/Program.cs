31aa5b7c11b8532e8dc05828af7f76621fae65cb
matheus21oliveira <matheus21oliveira@gmail.com>
2022-04-19 23:50:42 -0300

    aula 70

--- secao006/aula070/aula070/Program.cs
+++ secao006/aula070/aula070/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+namespace aula070 {
+    class Program {
+        static void Main(string[] args) {
+            int numero = int.Parse(Console.ReadLine());
+
+            Produto[] vetor = new Produto[numero]; 
+
+            for (int i = 0; i < numero; i++) {
+                string nome = Console.ReadLine();
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                vetor[i] = new Produto { Nome = nome, Preco = preco };
+
+            }
+
+            double soma = 0;
+
+            for (int i = 0; i < numero; i++) {
+                soma += vetor[i].Preco;
+            }
+
+            double media = soma / numero;
+
+            Console.WriteLine("AVERAGE PRICE = " + media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
