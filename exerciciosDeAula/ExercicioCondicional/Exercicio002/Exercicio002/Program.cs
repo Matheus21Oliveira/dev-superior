c4e5a8610efd8f94ade26193bcc19cba30437823
matheus21oliveira <matheus21oliveira@gmail.com>
2022-05-02 18:51:33 -0300

    exercicios condicionais

---
+++ exerciciosDeAula/ExercicioCondicional/Exercicio002/Exercicio002/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercicio002 {
+    class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine("Insira um número pra dizer se é impar ou par");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero%2 == 0) {
+                Console.WriteLine("PAR");
+            } else {
+                Console.WriteLine("IMPAR");
+            }
+        }
+    }
+}
---
+++ exerciciosDeAula/ExercicioCondicional/Exercicio003/Exercicio003/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio003 {
+    public class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine("Me dê o primeiro número pra ver se é multiplo: ");
+            int numero1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Me dê o segundo número pra ver se é multiplo: ");
+            int numero2 = int.Parse(Console.ReadLine());
+
+            if (numero1 % numero2 == 0 || numero2 % numero1 == 0) {
+                Console.WriteLine("Multiplo");
+            }
+            else {
+                Console.WriteLine("Não Multiplo");
+            }
+        }
+    }
+}
